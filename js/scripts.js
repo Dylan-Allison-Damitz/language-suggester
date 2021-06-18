26fb763cb03cc53972d08b9150ea83402c213a3f
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-06-18 16:26:37 -0700

    add functional js logic with if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    event.preventDefault();
+    
     const answerOne = parseInt($("input:radio[name=city]:checked").val());
     const answerTwo = parseInt($("input:radio[name=burrito]:checked").val());
-    const answerThree = parseInt($("input:radio[name=flavor]:checked").val());
-    const answerFour = parseInt($("input:radio[name=flavor]:checked").val());
-    const answerFive = parseInt($("input:radio[name=flavor]:checked").val());
+    const answerThree = parseInt($("input:radio[name=recreational]:checked").val());
+    const answerFour = parseInt($("input:radio[name=domicile]:checked").val());
+    const answerFive = parseInt($("input:radio[name=music]:checked").val());
+    
+    if(answerOne + answerTwo + answerThree + answerFour + answerFive <= 5 && 8) {
+      document.getElementById("javascript").style.display = "block";
+    } else if (answerOne + answerTwo + answerThree + answerFour + answerFive <= 9 && 14)
+      document.getElementById("ruby").style.display = "block";
+      else if (answerOne + answerTwo + answerThree + answerFour + answerFive == 15)
+      document.getElementById("python").style.display = "block";
+      
 
-    const result = answerOne + answerTwo + answerThree + answerFour + answerFive 
-  }
 
-}
+    
+    event.preventDefault();
+});
+});
