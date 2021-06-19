122275cc8f2e4b1319926277f9e0e96893dde342
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-06-18 18:00:05 -0700

    add bug fixes to if/else logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    
     const answerOne = parseInt($("input:radio[name=city]:checked").val());
     const answerTwo = parseInt($("input:radio[name=burrito]:checked").val());
     const answerThree = parseInt($("input:radio[name=recreational]:checked").val());
     const answerFour = parseInt($("input:radio[name=domicile]:checked").val());
     const answerFive = parseInt($("input:radio[name=music]:checked").val());
+    const result = answerOne + answerTwo + answerThree + answerFour + answerFive;
     
-    if(answerOne + answerTwo + answerThree + answerFour + answerFive < 8) {
-      document.getElementById("javascript").style.display = "block";
-    } else if (answerOne + answerTwo + answerThree + answerFour + answerFive <= 9 && 14)
-      document.getElementById("ruby").style.display = "block";
-      else if (answerOne + answerTwo + answerThree + answerFour + answerFive == 15)
-      document.getElementById("python").style.display = "block";
+    if(result < 9) {
+      document.getElementById("javascript").style.display ="block";
+      document.getElementById("hideAll").style.display ="none";
+    } else if (result >= 9 && result < 15) {
+      document.getElementById("ruby").style.display ="block";
+      document.getElementById("hideAll").style.display ="none";
+    } else if (result == 15) {
+      document.getElementById("python").style.display ="block";
+      document.getElementById("hideAll").style.display ="none";
+    }
+
+      
+
 
       
 
@@ -21,3 +28,4 @@ $(document).ready(function() {
     
 });
 });
+
